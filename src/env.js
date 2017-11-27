2201e71edbf736a83b55737613cbf0b4616053c5
Hossein <mhmardnai76@gmail.com>
2017-11-27 19:57:16 +0330

    change 2121 port to popular port 3000

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 export default {
-    Api_port: 2121, // Your Free Port For this server
+    Api_port: 3000, // Your Free Port For this server
     App_key: 'gglgm"7_"Tx?MT-eyY_.[EoElhX6Fl', // unique string for jwt
     DB_URL: 'mongodb://localhost:27017/test' // Your mongodb string
 }
